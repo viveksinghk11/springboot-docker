d7fc49c14ee9a5f7db56be83bb0d0178c0909883
Vivek Singh <vivek.singh@dyson.com>
2022-07-12 11:47:32 +0800

    Spring Boot with Docker

---
+++ src/test/java/com/apex/redhill/RedhillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.redhill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedhillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
